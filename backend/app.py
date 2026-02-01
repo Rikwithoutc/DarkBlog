68709d95387b1ce05fc106c35eca67e0c250f6d9
Rikwithoutc <dpratyus26@gmail.com>
2026-02-01 13:02:49 +0530

    Fixed db issue

--- backend/app.py
+++ backend/app.py
@@ -31,7 +31,7 @@ from application.routes import *
 
 if __name__ == "__main__":
 
-    # db.create_all()
+    db.create_all()
     # db.session.add(User(firstname="John", lastname="Doe", email="john@doe.com", password="123456", is_admin=True))
     # db.session.add(User(firstname="Rik", lastname="Dutta", email="rik@dutta.com", password="123456", is_admin=False))
     # db.session.commit()
